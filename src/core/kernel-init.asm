3f129390d3afc87c96c3d73ace6c2848643b2af1
macmade <macmade@72c75e04-ffb7-4f3e-8d67-181f0f877851>
2010-02-07 19:20:10 +0000

    Added the entry point file for the XEOS kernel.
    git-svn-id: file:///Users/macmade/Desktop/XEOS-SVN/trunk@194 72c75e04-ffb7-4f3e-8d67-181f0f877851

---
+++ src/core/kernel-init.asm
@@ -0,0 +1,81 @@
+;-------------------------------------------------------------------------------
+; XEOS - x86 Experimental Operating System
+; 
+; Copyright (C) 2010 Jean-David Gadina (macmade@eosgarden.com)
+; All rights reserved
+; 
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are met:
+; 
+;  -   Redistributions of source code must retain the above copyright notice,
+;      this list of conditions and the following disclaimer.
+;  -   Redistributions in binary form must reproduce the above copyright
+;      notice, this list of conditions and the following disclaimer in the
+;      documentation and/or other materials provided with the distribution.
+;  -   Neither the name of 'Jean-David Gadina' nor the names of its
+;      contributors may be used to endorse or promote products derived from
+;      this software without specific prior written permission.
+; 
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+; POSSIBILITY OF SUCH DAMAGE.
+;-------------------------------------------------------------------------------
+
+; $Id$
+
+;-------------------------------------------------------------------------------
+; XEOS kernel initialization
+; 
+; Note about compiling:
+;
+; This file has to be compiled as a ELF binary file.
+; 
+; The following compilers have been successfully tested:
+; 
+;       - NASM - The Netwide Assembler
+;       - YASM - The Yasm Modular Assembler
+; 
+; Other compilers have not been tested.
+; 
+; Examples:
+; 
+;       - nasm -f elf -o [boot.flp] [boot.asm]
+;       - yasm -f elf -o [boot.flp] [boot.asm]
+;-------------------------------------------------------------------------------
+
+; We are in 32 bits mode
+BITS    32
+
+; Makes the entry point visible to the linker
+global  XEOS.kernel.init
+
+; XEOS kernel is written in C, and is linked with the file
+extern  XEOS_kernel_main
+
+;-------------------------------------------------------------------------------
+; XEOS kernel entry point
+;-------------------------------------------------------------------------------
+XEOS.kernel.init:
+    
+    ; Sets the data segments
+    mov     ax,         0x10
+    mov     ds,         ax
+    mov     es,         ax
+    
+    ; Sets up the stack space
+    mov     ss,         ax
+    mov     esp,        0x90000
+    
+    ; Calls the XEOS kernel
+    call    XEOS_kernel_main
+    
+    ; Infinite loop
+    jmp     $
