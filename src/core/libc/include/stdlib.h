812ee5948b758e0a663ce6c1ba425ed54d1725d2
macmade <macmade@72c75e04-ffb7-4f3e-8d67-181f0f877851>
2010-02-10 21:30:53 +0000

    The header file stdlib.h should now be ok...
    git-svn-id: file:///Users/macmade/Desktop/XEOS-SVN/trunk@253 72c75e04-ffb7-4f3e-8d67-181f0f877851

--- src/core/libc/include/stdlib.h
+++ src/core/libc/include/stdlib.h
@@ -35,6 +35,202 @@
 #define __LIBC_STDLIB_H__
 #pragma once
 
+#include "private/null.h"
+#include "private/size_t.h"
 
+/**
+ * Value for status argument to exit indicating failure.
+ */
+#define EXIT_FAILURE    1
+
+/**
+ * Value for status argument to exit indicating success.
+ */
+#define EXIT_SUCCESS    0
+
+/**
+ * Maximum value returned by rand().
+ */
+#define RAND_MAX        32767
+
+/**
+ * Return type of div(). Structure having members:
+ * 
+ *      - int quot      quotient
+ *      - int rem       remainder
+ */
+typedef struct
+{
+    
+    int quot;
+    int rem;
+    
+} div_t;
+
+/**
+ * Return type of ldiv(). Structure having members:
+ * 
+ *      - long quot     quotient
+ *      - long rem      remainder
+ */
+typedef struct
+{
+    
+    long quot;
+    long rem;
+    
+} ldiv_t;
+
+/**
+ * Returns absolute value of n.
+ */
+int abs( int n );
+
+/**
+ * Returns absolute value of n.
+ */
+long labs( long n );
+
+/**
+ * Returns quotient and remainder of num/denom.
+ */
+div_t div( int num, int denom );
+
+/**
+ * Returns quotient and remainder of num/denom.
+ */
+ldiv_t ldiv( long num, long denom );
+
+/**
+ * Equivalent to strtod( s, ( char ** ) NULL) except that errno is not
+ * necessarily set on conversion error.
+ */
+double atof( const char * s );
+
+
+/**
+ * Equivalent to ( int )strtol( s, ( char ** ) NULL, 10 ) except that errno
+ * is not necessarily set on conversion error.
+ */
+int atoi( const char * s );
+
+/**
+ * Equivalent to strtol( s, ( char ** )NULL, 10 ) except that errno is not
+ * necessarily set on conversion error.
+ */
+long atol( const char * s );
+
+/**
+ * Converts initial characters (ignoring leading white space) of s to type
+ * double. If endp non-null, stores pointer to unconverted suffix in * endp.
+ * On overflow, sets errno to ERANGE and returns HUGE_VAL with the appropriate
+ * sign; on underflow, sets errno to ERANGE and returns zero; otherwise returns
+ * converted value.
+ */
+double strtod( const char * s, char ** endp );
+
+/**
+ * Converts initial characters (ignoring leading white space) of s to type long.
+ * If endp non-null,
+stores pointer to unconverted suffix in * endp.
+ * If base between 2 and 36, that base used for conversion; if zero, leading
+ * (after any sign) 0X or 0x implies hexadecimal, leading 0 (after any sign)
+ * implies octal, otherwise decimal assumed. Leading 0X or 0x permitted for
+ * base hexadecimal. On overflow, sets errno to ERANGE and returns LONG_MAX
+ * or LONG_MIN (as appropriate for sign); otherwise returns converted value.
+ */
+long strtol( const char * s, char ** endp, int base );
+
+/**
+ * As for strtol except result is unsigned long and value on overflow is
+ * ULONG_MAX.
+ */
+unsigned long strtoul( const char * s, char ** endp, int base );
+
+/**
+ * Returns pointer to zero-initialised newly-allocated space for an array of
+ * nobj objects each of size size, or NULL on error.
+ */
+void * calloc( size_t nobj, size_t size );
+
+/**
+ * Returns pointer to uninitialised newly-allocated space for an object of size
+ * size, or NULL on error.
+ */
+void * malloc( size_t size );
+
+/**
+ * Returns pointer to newly-allocated space for an object of size size,
+ * initialised, to minimum of old and new sizes, to existing contents of p
+ * (if non-null), or NULL on error. On success, old object deallocated,
+ * otherwise unchanged.
+ */
+void * realloc( void * p, size_t size );
+
+/**
+ * If p non-null, deallocates space to which it points.
+ */
+void free( void * p );
+
+/**
+ * Terminates program abnormally, by calling raise( SIGABRT ).
+ */
+void abort( void );
+
+/**
+ * Terminates program normally. Functions installed using atexit are called
+ * (in reverse order to that in which installed), open files are flushed, open
+ * streams are closed and control is returned to environment. status is
+ * returned to environment in implementation-dependent manner.
+ * Zero or EXIT_SUCCESS indicates successful termination and EXIT_FAILURE
+ * indicates unsuccessful termination. Implementations may define other values.
+ */
+void exit( int status );
+
+/**
+ * Registers fcn to be called when program terminates normally (or when main
+ * returns). Returns non-zero on failure.
+ */
+int atexit( void ( * fcm )( void ) );
+
+/**
+ * If s is not NULL, passes s to environment for execution, and returns status
+ * reported by command processor; if s is NULL, non-zero returned if environment
+ * has a command processor.
+ */
+int system( const char * s );
+
+/**
+ * Returns string associated with name name from implementation's environment,
+ * or NULL if no such string exists.
+ */
+char * getenv( const char * name );
+
+/**
+ * Searches ordered array base (of n objects each of size size) for item
+ * matching key according to comparison function cmp. cmp must return negative
+ * value if first argument is less than second, zero if equal and positive if
+ * greater. Items of base are assumed to be in ascending order (according to
+ * cmp). Returns a pointer to an item matching key, or NULL if none found.
+ */
+void * bsearch( const void * key, const void * base, size_t n, size_t size, int ( * cmp )( const void * keyval, const void * datum ) );
+
+/**
+ * Arranges into ascending order array base (of n objects each of size size)
+ * according to comparison function cmp. cmp must return negative value if first
+ * argument is less than second, zero if equal and positive if greater.
+ */
+void qsort( void * base, size_t n, size_t size, int ( * cmp )( const void *, const void * ) );
+
+/**
+ * Returns pseudo-random number in range 0 to RAND_MAX.
+ */
+int rand( void );
+
+/**
+ * Uses seed as seed for new sequence of pseudo-random numbers.
+ * Initial seed is 1.
+ */
+void srand( unsigned int seed );
 
 #endif /* __LIBC_STDLIB_H__ */
