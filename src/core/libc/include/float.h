03fd30ec1fb766be1c514a475ef19fed1f7f3af2
macmade <macmade@72c75e04-ffb7-4f3e-8d67-181f0f877851>
2010-02-18 13:27:23 +0000

    The header file float.h should now be ok.
    git-svn-id: file:///Users/macmade/Desktop/XEOS-SVN/trunk@312 72c75e04-ffb7-4f3e-8d67-181f0f877851

--- src/core/libc/include/float.h
+++ src/core/libc/include/float.h
@@ -39,7 +39,71 @@
 extern "C" {
 #endif
 
+/**
+ * Radix of floating-point representations
+ */
+#define FLT_RADIX       2
 
+/**
+ * Floating-point rounding mode
+ */
+#define FLT_ROUNDS      1
+
+/*******************************************************************************
+ * Where the prefix "FLT" pertains to type float, "DBL" to type double, and
+ * "LDBL" to type long double:
+ ******************************************************************************/
+
+/**
+ * Precision (in decimal digits)
+ */
+#define FLT_DIG         6
+#define DBL_DIG         15
+#define LDBL_DIG        15
+
+/**
+ * Smallest number x such that 1.0 + x != 1.0
+ */
+#define FLT_EPSILON     1.19209290e-07F
+#define DBL_EPSILON     2.2204460492503131e-16
+#define LDBL_EPSILON    2.2204460492503131e-16L
+
+/**
+ * Number of digits, base FLT_RADIX, in mantissa
+ */
+#define FLT_MANT_DIG    24
+#define DBL_MANT_DIG    53
+#define LDBL_MANT_DIG   53
+
+/**
+ * Maximum number
+ */
+#define FLT_MAX         3.40282347e+38F
+#define DBL_MAX         1.7976931348623157e+308
+#define LDBL_MAX        1.7976931348623157e+308L
+
+/**
+ * Largest positive integer exponent to which FLT_RADIX can be raised and
+ * remain representable
+ */
+#define FLT_MAX_EXP     128
+#define DBL_MAX_EXP     1024
+#define LDBL_MAX_EXP    1024
+
+/**
+ * Minimum normalised number
+ */
+#define FLT_MIN         1.17549435e-38F
+#define DBL_MIN         2.2250738585072014e-308
+#define LDBL_MIN        2.2250738585072014e-308L
+
+/**
+ * Smallest negative integer exponent to which FLT_RADIX can be raised and
+ * remain representable
+ */
+#define FLT_MIN_EXP     ( -125 )
+#define DBL_MIN_EXP     ( -1021 )
+#define LDBL_MIN_EXP    ( -1021 )
 
 #ifdef __cplusplus
 }
