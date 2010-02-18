5b8988a3bb1d2e6df95e37bee2bbc02efd90d81d
macmade <macmade@72c75e04-ffb7-4f3e-8d67-181f0f877851>
2010-02-18 22:46:56 +0000

    Added a header files for the syscalls, with the syscall C function.
    git-svn-id: file:///Users/macmade/Desktop/XEOS-SVN/trunk@347 72c75e04-ffb7-4f3e-8d67-181f0f877851

---
+++ src/core/kernel/include/syscalls.h
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * XEOS - x86 Experimental Operating System
+ * 
+ * Copyright (C) 2010 Jean-David Gadina (macmade@eosgarden.com)
+ * All rights reserved
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  -   Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  -   Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *  -   Neither the name of 'Jean-David Gadina' nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/* $Id$ */
+
+#ifndef __KERNEL_SYSCALLS_H__
+#define __KERNEL_SYSCALLS_H__
+#pragma once
+
+void syscall( unsigned char n );
+
+#define SYS_exit            1
+#define SYS_read            3
+#define SYS_write           4
+#define SYS_open            5
+#define SYS_close           6
+#define SYS_time            13
+#define SYS_brk             45
+#define SYS_reboot          88
+#define SYS_mmap            90
+
+#define SYS_test            100
+
+#define SYS_MAX_SYSCALL     256
+
+#endif /* __KERNEL_SYSCALLS_H__ */
