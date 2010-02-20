19353627dbba7967fc42c89a749819da263181e5
macmade <macmade@72c75e04-ffb7-4f3e-8d67-181f0f877851>
2010-02-20 18:26:23 +0000

    Added additionnal header files from the C standard library.
    git-svn-id: file:///Users/macmade/Desktop/XEOS-SVN/trunk@378 72c75e04-ffb7-4f3e-8d67-181f0f877851

--- src/core/libxeos/include/xeos.h
+++ src/core/libxeos/include/xeos.h
@@ -40,13 +40,16 @@
 #include <ctype.h>
 #include <errno.h>
 #include <float.h>
+#include <iso646.h>
 #include <limits.h>
 #include <locale.h>
 #include <math.h>
 #include <setjmp.h>
 #include <signal.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
