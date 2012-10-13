c3183a0a5fa07f43d084cf7f19fc3ede49ee2371
Macmade <macmade@eosgarden.com>
2012-10-13 19:04:14 +0200

    Constants now start with a @ instead of $.

--- source/boot/include/xeos.crt.controller.inc.s
+++ source/boot/include/xeos.crt.controller.inc.s
@@ -73,34 +73,34 @@
 ;-------------------------------------------------------------------------------
 
 ; Location of the video memory
-%define $XEOS.crt.controller.registers.data                 0x03D4
-%define $XEOS.crt.controller.registers.index                0x03D5
+%define @XEOS.crt.controller.registers.data                 0x03D4
+%define @XEOS.crt.controller.registers.index                0x03D5
 
 ; Indices for the index register
-%define $XEOS.crt.controller.horizontalTotal                0x0000
-%define $XEOS.crt.controller.endHorizontalDisplay           0x0001
-%define $XEOS.crt.controller.startHorizontalBlanking        0x0002
-%define $XEOS.crt.controller.endHorizontalBlanking          0x0003
-%define $XEOS.crt.controller.startHorizontalRetrace         0x0004
-%define $XEOS.crt.controller.endHorizontalRetrace           0x0005
-%define $XEOS.crt.controller.verticalTotal                  0x0006
-%define $XEOS.crt.controller.overflow                       0x0007
-%define $XEOS.crt.controller.presetRowScan                  0x0008
-%define $XEOS.crt.controller.maximumScanLine                0x0009
-%define $XEOS.crt.controller.cursorStart                    0x000A
-%define $XEOS.crt.controller.cursorEnd                      0x000B
-%define $XEOS.crt.controller.startAddressHigh               0x000C
-%define $XEOS.crt.controller.startAddressLow                0x000D
-%define $XEOS.crt.controller.cursorLocationHigh             0x000E
-%define $XEOS.crt.controller.cursorLocationLow              0x000F
-%define $XEOS.crt.controller.verticalRetraceStart           0x0010
-%define $XEOS.crt.controller.verticalRetraceEnd             0x0011
-%define $XEOS.crt.controller.verticalDisplayEnd             0x0012
-%define $XEOS.crt.controller.offset                         0x0013
-%define $XEOS.crt.controller.underlineLocation              0x0014
-%define $XEOS.crt.controller.startVerticalBlanking          0x0015
-%define $XEOS.crt.controller.endVerticalBlanking            0x0016
-%define $XEOS.crt.controller.crtcModeControl                0x0017
-%define $XEOS.crt.controller.lineCompare                    0x0018
+%define @XEOS.crt.controller.horizontalTotal                0x0000
+%define @XEOS.crt.controller.endHorizontalDisplay           0x0001
+%define @XEOS.crt.controller.startHorizontalBlanking        0x0002
+%define @XEOS.crt.controller.endHorizontalBlanking          0x0003
+%define @XEOS.crt.controller.startHorizontalRetrace         0x0004
+%define @XEOS.crt.controller.endHorizontalRetrace           0x0005
+%define @XEOS.crt.controller.verticalTotal                  0x0006
+%define @XEOS.crt.controller.overflow                       0x0007
+%define @XEOS.crt.controller.presetRowScan                  0x0008
+%define @XEOS.crt.controller.maximumScanLine                0x0009
+%define @XEOS.crt.controller.cursorStart                    0x000A
+%define @XEOS.crt.controller.cursorEnd                      0x000B
+%define @XEOS.crt.controller.startAddressHigh               0x000C
+%define @XEOS.crt.controller.startAddressLow                0x000D
+%define @XEOS.crt.controller.cursorLocationHigh             0x000E
+%define @XEOS.crt.controller.cursorLocationLow              0x000F
+%define @XEOS.crt.controller.verticalRetraceStart           0x0010
+%define @XEOS.crt.controller.verticalRetraceEnd             0x0011
+%define @XEOS.crt.controller.verticalDisplayEnd             0x0012
+%define @XEOS.crt.controller.offset                         0x0013
+%define @XEOS.crt.controller.underlineLocation              0x0014
+%define @XEOS.crt.controller.startVerticalBlanking          0x0015
+%define @XEOS.crt.controller.endVerticalBlanking            0x0016
+%define @XEOS.crt.controller.crtcModeControl                0x0017
+%define @XEOS.crt.controller.lineCompare                    0x0018
 
 %endif
