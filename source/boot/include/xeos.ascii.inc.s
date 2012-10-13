c76ab7d320775f2fec790a4a5557a93c4ed52899
Macmade <macmade@eosgarden.com>
2012-10-13 19:03:58 +0200

    Constants now start with a @ instead of $.

--- source/boot/include/xeos.ascii.inc.s
+++ source/boot/include/xeos.ascii.inc.s
@@ -69,43 +69,43 @@
 %define __XEOS_ASCII_INC_ASM__
 
 ; Control characters
-%define $ASCII.NUL      0x00
-%define $ASCII.SOH      0x01
-%define $ASCII.STX      0x02
-%define $ASCII.ETX      0x03
-%define $ASCII.EOT      0x04
-%define $ASCII.ENQ      0x05
-%define $ASCII.ACK      0x06
-%define $ASCII.BEL      0x07
-%define $ASCII.BS       0x08
-%define $ASCII.TAB      0x09
-%define $ASCII.LF       0x0A
-%define $ASCII.VT       0x0B
-%define $ASCII.FF       0x0C
-%define $ASCII.CR       0x0D
-%define $ASCII.SO       0x0E
-%define $ASCII.SI       0x0F
-%define $ASCII.DLE      0x10
-%define $ASCII.DC1      0x11
-%define $ASCII.DC2      0x12
-%define $ASCII.DC3      0x13
-%define $ASCII.DC4      0x14
-%define $ASCII.NAK      0x15
-%define $ASCII.SYN      0x16
-%define $ASCII.ETB      0x17
-%define $ASCII.CAN      0x18
-%define $ASCII.EM       0x19
-%define $ASCII.SUB      0x1A
-%define $ASCII.ESC      0x1B
-%define $ASCII.FS       0x1C
-%define $ASCII.GS       0x1D
-%define $ASCII.RS       0x1E
-%define $ASCII.US       0x1F
+%define @ASCII.NUL      0x00
+%define @ASCII.SOH      0x01
+%define @ASCII.STX      0x02
+%define @ASCII.ETX      0x03
+%define @ASCII.EOT      0x04
+%define @ASCII.ENQ      0x05
+%define @ASCII.ACK      0x06
+%define @ASCII.BEL      0x07
+%define @ASCII.BS       0x08
+%define @ASCII.TAB      0x09
+%define @ASCII.LF       0x0A
+%define @ASCII.VT       0x0B
+%define @ASCII.FF       0x0C
+%define @ASCII.CR       0x0D
+%define @ASCII.SO       0x0E
+%define @ASCII.SI       0x0F
+%define @ASCII.DLE      0x10
+%define @ASCII.DC1      0x11
+%define @ASCII.DC2      0x12
+%define @ASCII.DC3      0x13
+%define @ASCII.DC4      0x14
+%define @ASCII.NAK      0x15
+%define @ASCII.SYN      0x16
+%define @ASCII.ETB      0x17
+%define @ASCII.CAN      0x18
+%define @ASCII.EM       0x19
+%define @ASCII.SUB      0x1A
+%define @ASCII.ESC      0x1B
+%define @ASCII.FS       0x1C
+%define @ASCII.GS       0x1D
+%define @ASCII.RS       0x1E
+%define @ASCII.US       0x1F
 
 ; Space character
-%define $ASCII.SPC      0x20
+%define @ASCII.SPC      0x20
 
 ; New line character (CRLF)
-%define $ASCII.NL   $ASCII.CR, $ASCII.LF
+%define @ASCII.NL   @ASCII.CR, @ASCII.LF
 
 %endif
