c8c76d040e54e6f598331cbe5ec7c090322df8bf
Macmade <macmade@eosgarden.com>
2012-10-14 15:08:40 +0200

    Added a boot test file.

---
+++ source/boot/test.s
@@ -0,0 +1,158 @@
+;-------------------------------------------------------------------------------
+; XEOS - X86 Experimental Operating System
+; 
+; Copyright (c) 2010-2012, Jean-David Gadina <macmade@eosgarden.com>
+; All rights reserved.
+; 
+; XEOS Software License - Version 1.0 - December 21, 2012
+; 
+; Permission is hereby granted, free of charge, to any person or organisation
+; obtaining a copy of the software and accompanying documentation covered by
+; this license (the "Software") to deal in the Software, with or without
+; modification, without restriction, including without limitation the rights
+; to use, execute, display, copy, reproduce, transmit, publish, distribute,
+; modify, merge, prepare derivative works of the Software, and to permit
+; third-parties to whom the Software is furnished to do so, all subject to the
+; following conditions:
+; 
+;       1.  Redistributions of source code, in whole or in part, must retain the
+;           above copyright notice and this entire statement, including the
+;           above license grant, this restriction and the following disclaimer.
+; 
+;       2.  Redistributions in binary form must reproduce the above copyright
+;           notice and this entire statement, including the above license grant,
+;           this restriction and the following disclaimer in the documentation
+;           and/or other materials provided with the distribution, unless the
+;           Software is distributed by the copyright owner as a library.
+;           A "library" means a collection of software functions and/or data
+;           prepared so as to be conveniently linked with application programs
+;           (which use some of those functions and data) to form executables.
+; 
+;       3.  The Software, or any substancial portion of the Software shall not
+;           be combined, included, derived, or linked (statically or
+;           dynamically) with software or libraries licensed under the terms
+;           of any GNU software license, including, but not limited to, the GNU
+;           General Public License (GNU/GPL) or the GNU Lesser General Public
+;           License (GNU/LGPL).
+; 
+;       4.  All advertising materials mentioning features or use of this
+;           software must display an acknowledgement stating that the product
+;           includes software developed by the copyright owner.
+; 
+;       5.  Neither the name of the copyright owner nor the names of its
+;           contributors may be used to endorse or promote products derived from
+;           this software without specific prior written permission.
+; 
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNER AND CONTRIBUTORS "AS IS"
+; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+; THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+; PURPOSE, TITLE AND NON-INFRINGEMENT ARE DISCLAIMED.
+; 
+; IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR ANYONE DISTRIBUTING
+; THE SOFTWARE BE LIABLE FOR ANY CLAIM, DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+; WHETHER IN ACTION OF CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF OR IN CONNECTION WITH
+; THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE, EVEN IF ADVISED
+; OF THE POSSIBILITY OF SUCH DAMAGE.
+;-------------------------------------------------------------------------------
+
+; $Id$
+
+; Location at which we were loaded by the first stage bootloader (0x50:0)
+ORG     0
+
+; We are in 16 bits mode
+BITS    16
+
+; Jumps to the entry point
+start: jmp main
+
+;-------------------------------------------------------------------------------
+; Includes
+;-------------------------------------------------------------------------------
+%include "XEOS.constants.inc.s"       ; General constants
+%include "XEOS.macros.inc.s"          ; General macros
+%include "BIOS.int.inc.s"             ; BIOS interrupts
+%include "BIOS.video.inc.16.s"        ; BIOS video services
+%include "XEOS.ascii.inc.s"           ; ASCII table
+
+;-------------------------------------------------------------------------------
+; Definitions & Macros
+;-------------------------------------------------------------------------------
+
+; Prints a new line with a message, prefixed by the prompt
+%macro @XEOS.boot.test.print 1
+    
+    @BIOS.video.print    $XEOS.boot.test.prompt
+    @BIOS.video.print    %1
+    @BIOS.video.print    $XEOS.boot.test.nl
+    
+%endmacro
+
+;-------------------------------------------------------------------------------
+; Variables definition
+;-------------------------------------------------------------------------------
+
+$XEOS.boot.test.nl          db  @ASCII.NL,  @ASCII.NUL
+$XEOS.boot.test.prompt      db  '[ XEOS ]> ', @ASCII.NUL
+$XEOS.boot.test.greet1      db  'Entering the boot test program', @ASCII.NUL
+$XEOS.boot.test.greet2      db  'Press any key to reboot', @ASCII.NUL
+
+;-------------------------------------------------------------------------------
+; XEOS boot test program
+; 
+; Note about compiling:
+;
+; This file has to be compiled as a flat-form binary file.
+; 
+; The following compilers have been successfully tested:
+; 
+;       - NASM - The Netwide Assembler
+;       - YASM - The Yasm Modular Assembler
+; 
+; Other compilers have not been tested.
+; 
+; Examples:
+; 
+;       - nasm -f bin -o [boot.flp] [boot.s]
+;       - yasm -f bin -o [boot.flp] [boot.s]
+;-------------------------------------------------------------------------------
+main:
+    
+    ; Clears the interrupts as we are setting-up the segments and stack space
+    cli
+    
+    ; Sets the data and extra segments to where we were loaded by the second
+    ; stage bootloader (0x1500), so we don't have to add 0x1500 to all our data
+    mov     ax,         0x1500
+    mov     ds,         ax
+    mov     es,         ax
+    mov     fs,         ax
+    mov     gs,         ax
+    
+    ; Sets up the of stack space
+    xor     ax,         ax
+    mov     ss,         ax
+    mov     sp,         0xFFFF
+    
+    ; Restores the interrupts
+    sti
+    
+    ; Prints the welcome message
+    @XEOS.boot.test.print $XEOS.boot.test.greet1
+    @XEOS.boot.test.print $XEOS.boot.test.greet2
+    
+    ; Waits for a key press
+    xor     ax,         ax
+    @BIOS.int.keyboard
+    
+    ; Reboot the computer
+    @BIOS.int.reboot
+    
+    ; Halts the system
+    cli
+    hlt
+    
