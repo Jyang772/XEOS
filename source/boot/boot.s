df3ab3b6b747cc70e6b973b77100e23692403cad
Macmade <macmade@eosgarden.com>
2012-10-16 01:46:28 +0200

    Cleanup...

--- source/boot/boot.s
+++ source/boot/boot.s
@@ -155,8 +155,8 @@ $XEOS.boot.stage2.msg.cpu.instructions.32       db  "32 bits", @ASCII.NUL
 $XEOS.boot.stage2.msg.cpu.instructions.64       db  "64 bits", @ASCII.NUL
 $XEOS.boot.stage2.msg.kernel.load               db  "Loading the kernel image:                              ", @ASCII.NUL
 $XEOS.boot.stage2.msg.fat12.root                db  "            - Loading the FAT-12 directory into memory:          0x0050:7900", @ASCII.NUL
-$XEOS.boot.stage2.msg.fat12.find                db  "            - Locating the kernel file:                          ", @ASCII.NUL
-$XEOS.boot.stage2.msg.fat12.load                db  "            - Loading the kernel into memory:                    0x1000:0000", @ASCII.NUL
+$XEOS.boot.stage2.msg.fat12.find                db  "            - Locating the kernel image:                         ", @ASCII.NUL
+$XEOS.boot.stage2.msg.fat12.load                db  "            - Loading the kernel image into memory:              0x1000:0000", @ASCII.NUL
 $XEOS.boot.stage2.msg.kernel.verify.32          db  "Verifiying the kernel image (ELF-32):                  ", @ASCII.NUL
 $XEOS.boot.stage2.msg.kernel.verify.64          db  "Verifiying the kernel image (ELF-64):                  ", @ASCII.NUL
 $XEOS.boot.stage2.msg.gdt                       db  "Installing the GDT:                                    ", @ASCII.NUL
