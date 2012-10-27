7fed23323a61445fe5c64e565e81380ff47ba9cf
Macmade <macmade@eosgarden.com>
2012-10-27 20:39:29 +0200

    locale.h

--- source/core/libc/locale/localeconv.c
+++ source/core/libc/locale/localeconv.c
@@ -61,58 +61,9 @@
 
 /* $Id$ */
 
-#ifndef __LIBC_LOCALE_H__
-#define __LIBC_LOCALE_H__
-#pragma once
+#include "locale.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <libc/__null.h>
-
-struct lconv
+struct lconv * localeconv( void )
 {
-    char * decimal_point;
-    char * thousands_sep;
-    char * grouping;
-    char * mon_decimal_point;
-    char * mon_thousands_sep;
-    char * mon_grouping;
-    char * positive_sign;
-    char * negative_sign;
-    char * currency_symbol;
-    char * int_curr_symbol;
-    char frac_digits;
-    char p_cs_precedes;
-    char n_cs_precedes;
-    char p_sep_by_space;
-    char n_sep_by_space;
-    char p_sign_posn;
-    char n_sign_posn;
-    char int_frac_digits;
-    char int_p_cs_precedes;
-    char int_n_cs_precedes;
-    char int_p_sep_by_space;
-    char int_n_sep_by_space;
-    char int_p_sign_posn;
-    char int_n_sign_posn;
-    
-    char __pad_0[ 2 ];
-};
-
-#define LC_ALL              0
-#define LC_COLLATE          1
-#define LC_CTYPE            2
-#define LC_MONETARY         3
-#define LC_NUMERIC          4
-#define LC_TIME             5
-
-char * setlocale( int category, const char * locale );
-struct lconv * localeconv( void );
-
-#ifdef __cplusplus
+    return NULL;
 }
-#endif
-
-#endif /* __LIBC_LOCALE_H__ */
