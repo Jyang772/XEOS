08171abda1c47716adb48cb2d7ec7ed4c5ec2ef3
Macmade <macmade@eosgarden.com>
2012-10-27 22:05:57 +0200

    stdio.h

--- source/core/libc/stdio/snprintf.c
+++ source/core/libc/stdio/snprintf.c
@@ -61,101 +61,13 @@
 
 /* $Id$ */
 
-#ifndef __LIBC_STDIO_H__
-#define __LIBC_STDIO_H__
-#pragma once
+#include "stdlib.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <libc/__size_t.h>
-#include <libc/__null.h>
-#include <libc/__va_list.h>
-
-typedef long long           fpos_t;
-
-typedef struct
+int snprintf( char * restrict s, size_t n, const char * restrict format, ... )
 {
-    unsigned int reserved[ 64 ];
+    ( void )s;
+    ( void )n;
+    ( void )format;
+    
+    return 0;
 }
-FILE;
-
-#define _IOFBF              0
-#define _IOLBF              1
-#define _IONBF              2
-
-#define BUFSIZ              1024
-#define EOF                 ( -1 )
-
-#define FOPEN_MAX           20
-#define FILENAME_MAX        1024
-
-#define L_tmpnam            1024
-
-#define SEEK_SET            0
-#define SEEK_CUR            1
-#define SEEK_END            2
-
-#define TMP_MAX             0xFFFF
-
-#define stderr              __libc_stderr_p
-#define stdin               __libc_stdin_p
-#define stdout              __libc_stdout_p
-
-extern FILE * __libc_stderr_p;
-extern FILE * __libc_stdin_p;
-extern FILE * __libc_stdout_p;
-
-int remove( const char * filename );
-int rename( const char * old, const char * new );
-FILE * tmpfile( void );
-char * tmpnam( char * s );
-int fclose( FILE * stream );
-int fflush( FILE * stream );
-FILE * fopen( const char * restrict filename, const char * restrict mode );
-FILE * freopen( const char * restrict filename, const char * restrict mode, FILE * restrict stream );
-void setbuf( FILE * restrict stream, char * restrict buf );
-int setvbuf( FILE * restrict stream, char * restrict buf, int mode, size_t size );
-int fprintf( FILE * restrict stream, const char * restrict format, ... );
-int fscanf( FILE * restrict stream, const char * restrict format, ... );
-int printf( const char * restrict format, ... );
-int scanf( const char * restrict format, ... );
-int snprintf( char * restrict s, size_t n, const char * restrict format, ... );
-int sprintf( char * restrict s, const char * restrict format, ... );
-int sscanf( const char * restrict s, const char * restrict format, ... );
-int vfprintf( FILE * restrict stream, const char * restrict format, va_list arg );
-int vfscanf( FILE * restrict stream, const char * restrict format, va_list arg );
-int vprintf( const char * restrict format, va_list arg );
-int vscanf( const char * restrict format, va_list arg );
-int vsnprintf( char * restrict s, size_t n, const char * restrict format, va_list arg );
-int vsprintf( char * restrict s, const char * restrict format, va_list arg );
-int vsscanf( const char * restrict s, const char * restrict format, va_list arg );
-int fgetc( FILE * stream );
-char * fgets( char * restrict s, int n, FILE * restrict stream );
-int fputc( int c, FILE * stream );
-int fputs( const char * restrict s, FILE * restrict stream );
-int getc( FILE * stream );
-int getchar( void );
-char * gets( char * s );
-int putc( int c, FILE * stream );
-int putchar( int c );
-int puts( const char * s );
-int ungetc( int c, FILE * stream );
-size_t fread( void * restrict ptr, size_t size, size_t nmemb, FILE * restrict stream );
-size_t fwrite( const void * restrict ptr, size_t size, size_t nmemb, FILE * restrict stream );
-int fgetpos( FILE * restrict stream, fpos_t * restrict pos );
-int fseek( FILE * stream, long int offset, int whence );
-int fsetpos( FILE * stream, const fpos_t * pos );
-long int ftell( FILE * stream );
-void rewind( FILE * stream );
-void clearerr( FILE * stream );
-int feof( FILE * stream );
-int ferror( FILE * stream );
-void perror( const char * s );
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __LIBC_STDIO_H__ */
