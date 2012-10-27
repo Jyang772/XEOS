3e1ddd6b6f58a1d9a5997c05301f63616057e01f
Macmade <macmade@eosgarden.com>
2012-10-27 16:14:01 +0200

    limits.h

--- source/core/libc/include/limits.h
+++ source/core/libc/include/limits.h
@@ -69,7 +69,52 @@
 extern "C" {
 #endif
 
+#define SCHAR_MAX           0x7F
+#define SHRT_MAX            0x7FFF
+#define INT_MAX             0x7FFFFFFF
 
+#ifdef __LP64__
+    
+    #define LONG_MAX        0x7FFFFFFFFFFFFFFFL
+    
+#else
+    
+    #define LONG_MAX        0x7FFFFFFF
+    
+#endif
+
+#define SCHAR_MIN           ( -SCHAR_MAX - 1  )
+#define SHRT_MIN            ( -SHRT_MAX  - 1  )
+#define INT_MIN             ( -INT_MAX   - 1  )
+#define LONG_MIN            ( -LONG_MAX  - 1L )
+
+#define UCHAR_MAX           ( SCHAR_MAX * 2   + 1   )
+#define USHRT_MAX           ( SHRT_MAX  * 2   + 1   )
+#define UINT_MAX            ( INT_MAX   * 2U  + 1U  )
+#define ULONG_MAX           ( LONG_MAX  * 2UL + 1UL )
+
+#if __STDC_VERSION__ >= 199901
+
+    #define LLONG_MAX       0x7FFFFFFFFFFFFFFFL
+    #define LLONG_MIN       ( -LLONG_MAX - 1LL )
+    #define ULLONG_MAX      ( LLONG_MAX * 2ULL + 1ULL )
+
+#endif
+
+#ifdef __CHAR_UNSIGNED__
+    
+    #define CHAR_MIN        0x00
+    #define CHAR_MAX        UCHAR_MAX
+    
+#else
+    
+    #define CHAR_MIN        SCHAR_MIN
+    #define CHAR_MAX        SCHAR_MAX
+    
+#endif
+
+#define CHAR_BIT            0x08
+#define MB_LEN_MAX          0x01
 
 #ifdef __cplusplus
 }
