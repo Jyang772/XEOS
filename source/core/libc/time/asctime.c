9cb260160412a71451b55ac013ae748c6794cac6
Macmade <macmade@eosgarden.com>
2012-10-27 20:54:07 +0200

    time.h

--- source/core/libc/time/asctime.c
+++ source/core/libc/time/asctime.c
@@ -61,47 +61,11 @@
 
 /* $Id$ */
 
-#ifndef __LIBC_TIME_H__
-#define __LIBC_TIME_H__
-#pragma once
+#include "time.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <libc/__null.h>
-#include <libc/__size_t.h>
-
-#define CLOCKS_PER_SEC          1000000
-
-typedef unsigned long           clock_t;
-typedef long                    time_t;
-
-struct tm
+char * asctime( const struct tm * timeptr )
 {
-    int tm_sec;
-    int tm_min;
-    int tm_hour;
-    int tm_mday;
-    int tm_mon;
-    int tm_year;
-    int tm_wday;
-    int tm_yday;
-    int tm_isdst;
-};
-
-clock_t clock( void );
-double difftime( time_t time1, time_t time0 );
-time_t mktime( struct tm * timeptr );
-time_t time( time_t * timer );
-char * asctime( const struct tm * timeptr );
-char * ctime( const time_t * timer );
-struct tm * gmtime( const time_t * timer );
-struct tm * localtime( const time_t * timer );
-size_t strftime( char * restrict s, size_t maxsize, const char * restrict format, const struct tm * restrict timeptr );
-
-#ifdef __cplusplus
+    ( void )timeptr;
+    
+    return NULL;
 }
-#endif
-
-#endif /* __LIBC_TIME_H__ */
