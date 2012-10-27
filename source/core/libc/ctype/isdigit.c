e2ead78db21ec49c8fc92a67a1d43d924f1f2f4f
Macmade <macmade@eosgarden.com>
2012-10-27 16:16:17 +0200

    ctype.h

--- source/core/libc/ctype/isdigit.c
+++ source/core/libc/ctype/isdigit.c
@@ -61,31 +61,9 @@
 
 /* $Id$ */
 
-#ifndef __LIBC_CTYPE_H__
-#define __LIBC_CTYPE_H__
-#pragma once
+#include "ctype.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int isalnum( int c );
-int isalpha( int c );
-int isblank( int c );
-int iscntrl( int c );
-int isdigit( int c );
-int isgraph( int c );
-int islower( int c );
-int isprint( int c );
-int ispunct( int c );
-int isspace( int c );
-int isupper( int c );
-int isxdigit( int c );
-int tolower( int c );
-int toupper( int c );
-
-#ifdef __cplusplus
+int isdigit( int c )
+{
+    return c > 0x2F && c < 0x3A;
 }
-#endif
-
-#endif /* __LIBC_CTYPE_H__ */
