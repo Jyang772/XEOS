05bade60fa770ad1e28de9772a6ab80b3bb17b49
Macmade <macmade@eosgarden.com>
2012-10-27 16:12:10 +0200

    setjmp.h

---
+++ source/core/libc/setjmp/longjmp.c
@@ -0,0 +1,9 @@
+//
+//  longjmp.c
+//  XEOS
+//
+//  Created by Macmade on 27.10.2012.
+//  Copyright (c) 2012 eosgarden. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ source/core/libc/setjmp/setjmp.c
@@ -0,0 +1,9 @@
+//
+//  setjmp.c
+//  XEOS
+//
+//  Created by Macmade on 27.10.2012.
+//  Copyright (c) 2012 eosgarden. All rights reserved.
+//
+
+#include <stdio.h>
