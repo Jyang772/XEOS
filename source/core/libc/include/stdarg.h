5533eae3a7f7f6ded11161ae3d35e272f25292a6
Macmade <macmade@eosgarden.com>
2012-10-30 22:03:07 +0100

    stdarg cleanup. Clang builtins are now used whenever possible.

--- source/core/libc/include/stdarg.h
+++ source/core/libc/include/stdarg.h
@@ -76,10 +76,21 @@ extern "C" {
 typedef __XEOS_va_list                  va_list;
 #endif
 
+#ifdef __clang__
+
+#define va_start( _ap_, _lastarg_ )     __builtin_va_start( _ap_, _lastarg_ )
+#define va_arg( _ap_, _type_ )          __builtin_va_arg( _ap_, _type_ )
+#define va_copy( _dst_, _src_ )         __builtin_va_copy( _dts_, _src_ )
+#define va_end( _ap_ )                  __builtin_va_end( _ap_ )
+
+#else
+
 #define va_start( _ap_, _lastarg_ )     _ap_ = ( char * ) & _lastarg_ + sizeof( int )
 #define va_arg( _ap_, _type_ )          *( _type_ * )( _ap_ += sizeof( _type_ ), _ap_ - sizeof( _type_ ) )
 #define va_copy( _dst_, _src_ )         ( ( void )( ( _dst_ ) = ( _src_ ) ) )
-#define va_end( _ap_ )                  _ap_ = 0
+#define va_end( _ap_ )                  _ap_ = ( va_list )0
+
+#endif
 
 #ifdef __cplusplus
 }
